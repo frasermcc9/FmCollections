1fd461eb8f10f0ec4986433f6ad6a4eb542c04e5
Fraser McCallum <fraser@themccallums.net.nz>
2020-10-29 19:57:17 +1300

    Added license header

--- src/test/java/collections/FmMapTest.java
+++ src/test/java/collections/FmMapTest.java
@@ -1,19 +1,44 @@
+/*
+ * Copyright 2020 Fraser McCallum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package collections;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test suite for Map Extension.
+ *
+ * @author Fraser McCallum
+ * @since 1.0
+ */
 class FmMapTest {
 
     private static FmMap<String, Integer> generateMap() {
-        return new FmMap<String, Integer>().set("Bob", 15).set("Harry", 19).set("Mark", 24);
+        return new FmMapImpl<String, Integer>().set("Bob", 15).set("Harry", 19).set("Mark", 24);
     }
 
     private static FmMap<String, Integer> generateAltMap() {
-        return new FmMap<String, Integer>().set("Bob", 15).set("Julia", 19).set("Mark", 35);
+        return new FmMapImpl<String, Integer>().set("Bob", 15).set("Julia", 19).set("Mark", 35);
     }
 
+    /**
+     * Tests the random method on the map extension.
+     */
     @Test
     void random() {
         var result = generateMap().random();
@@ -126,7 +151,7 @@ class FmMapTest {
 
     @Test
     void set() {
-        var map = new FmMap<>();
+        var map = new FmMapImpl<>();
         assertEquals(0, map.size());
 
         map.set("K1", "V1").set("K2", "V2");
